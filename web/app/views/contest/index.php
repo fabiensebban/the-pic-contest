45fd8a81ef73923f7844eba8c1124454f40225c5
Fabian Sebban <fa.sebban@gmail.com>
2016-01-13 10:02:45 +0100

    Second commit for get the correct base

---
+++ web/app/views/contest/index.php
@@ -0,0 +1,134 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8">
+            <h3 class="text-center">
+                Photos participants
+            </h3>
+        </div>
+        <div class="col-md-4">
+            <div class="btn-group btn-group-md">
+                 
+                <button class="btn btn-default" type="button">
+                    <em class="glyphicon glyphicon-align-left"></em> Left
+                </button> 
+                <button class="btn btn-default" type="button">
+                    <em class="glyphicon glyphicon-align-center"></em> Center
+                </button> 
+                <button class="btn btn-default" type="button">
+                    <em class="glyphicon glyphicon-align-right"></em> Right
+                </button> 
+                <button class="btn btn-default" type="button">
+                    <em class="glyphicon glyphicon-align-justify"></em> Justify
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <h3 class="text-center">
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+        </div>
+        <div class="col-md-8">
+            <div class="btn-group">
+                 
+                <button class="btn btn-default">
+                    Action
+                </button> 
+                <button data-toggle="dropdown" class="btn btn-default dropdown-toggle">
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li>
+                        <a href="#">Action</a>
+                    </li>
+                    <li class="disabled">
+                        <a href="#">Another action</a>
+                    </li>
+                    <li class="divider">
+                    </li>
+                    <li>
+                        <a href="#">Something else here</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ul class="pagination">
+                <li>
+                    <a href="#">Prev</a>
+                </li>
+                <li>
+                    <a href="#">1</a>
+                </li>
+                <li>
+                    <a href="#">2</a>
+                </li>
+                <li>
+                    <a href="#">3</a>
+                </li>
+                <li>
+                    <a href="#">4</a>
+                </li>
+                <li>
+                    <a href="#">5</a>
+                </li>
+                <li>
+                    <a href="#">Next</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
